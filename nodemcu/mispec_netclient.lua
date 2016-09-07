8c2f9d51b72d0d2c44858c17d84612cb2abfcd3c
Luis Fonseca <luis@pusher.com>
2016-09-08 00:06:33 +0100

    Test TLS first as it uses more heap

--- nodemcu/mispec_netclient.lua
+++ nodemcu/mispec_netclient.lua
@@ -10,8 +10,7 @@ describe('The net module (client)', function(it)
     it:should('create connection', function()
         net.createConnection(net.TCP, 0)
         net.createConnection(net.TCP, 1)
-        --net.createConnection(net.UDP, 0)
-        --net.createConnection(net.UPD, 1)
+        net.createConnection(net.UDP)
     end)
 
     it:should('resolve dns', function()
@@ -31,31 +30,6 @@ describe('The net module (client)', function(it)
         eventually(function() ok(eq(connected, true)) end)
 
         andThen(function() sk:close() end)
-    end)
-
-    it:should('communicate to remote host via TCP', function()
-        local connected = false
-        local sent = false
-        local received = nil
-        local sk = net.createConnection(net.TCP, 0)
-        sk:on('connection', function(_, c) connected = true end)
-        sk:connect(80, "www.example.com")
-
-        eventually(function() ok(eq(connected, true)) end)
-
-        andThen(function()
-            -- configure events
-            sk:on('sent', function() sent = true end)
-            sk:on('receive', function(_, data) received = data end)
-
-            -- send message
-            sk:send('GET / HTTP/1.1\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n')
-        end)
-
-        eventually(function() ok(eq(sent, true)) end)
-        eventually(function() ok(received ~= nil) end)
-
-        andThen(function() sk:close() sk = nil end)
     end)
 
     it:should('communicate to remote host via TCP with TLS', function()
@@ -83,6 +57,30 @@ describe('The net module (client)', function(it)
         andThen(function() sk:close() sk = nil end)
     end)
 
+    it:should('communicate to remote host via TCP', function()
+        local connected = false
+        local sent = false
+        local received = nil
+        local sk = net.createConnection(net.TCP, 0)
+        sk:on('connection', function(_, c) connected = true end)
+        sk:connect(80, "www.example.com")
+
+        eventually(function() ok(eq(connected, true)) end)
+
+        andThen(function()
+            -- configure events
+            sk:on('sent', function() sent = true end)
+            sk:on('receive', function(_, data) received = data end)
+
+            -- send message
+            sk:send('GET / HTTP/1.1\r\nConnection: keep-alive\r\nAccept: */*\r\n\r\n')
+        end)
+
+        eventually(function() ok(eq(sent, true)) end)
+        eventually(function() ok(received ~= nil) end)
+
+        andThen(function() sk:close() sk = nil end)
+    end)
 end)
 
 mispec.run()
